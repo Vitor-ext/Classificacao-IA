0fe98849797a9f1141d4818c94fbd1b7327e7b9e
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-11-23 16:59:55 -0300

    IA

---
+++ app/classificacao.py
@@ -0,0 +1,38 @@
+# classificacao.py
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+
+# Carregando o conjunto de dados de câncer de mama
+data = load_breast_cancer()
+X = data.data  # Features
+y = data.target  # Target variable (0 - benigno, 1 - maligno)
+
+# Dividindo o conjunto de dados em treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Escalando os dados
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+
+# Criando e treinando um modelo de regressão logística
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train_scaled, y_train)
+
+# Aplicando a transformação do scaler nos dados de teste
+X_test_scaled = scaler.transform(X_test)
+
+# Realizando previsões no conjunto de teste
+y_pred = model.predict(X_test_scaled)
+
+# Avaliando o desempenho do modelo
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+class_report = classification_report(y_test, y_pred)
+
+# Exibindo os resultados
+print("Accuracy:", accuracy)
+print("\nConfusion Matrix:\n", conf_matrix)
+print("\nClassification Report:\n", class_report)
---
+++ app/server.py
@@ -0,0 +1,34 @@
+from flask import render_template, request, jsonify
+from app import app
+from app.classificacao import LogisticRegression
+
+model = LogisticRegression()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    if request.method == 'POST':
+        # Obtendo os dados do formulário HTML
+        features = [float(request.form[f'feature{i}']) for i in range(1, 31)]
+
+        # Realizando a previsão usando o modelo treinado
+        prediction = model.predict([features])
+
+        # Convertendo o resultado para uma string legível
+        result = "Maligno" if prediction[0] == 1 else "Benigno"
+
+        # Criando um dicionário para retornar os resultados como JSON
+        result_dict = {
+            'result': result,
+            'accuracy': float(model.accuracy),
+            'conf_matrix': model.conf_matrix.tolist(),
+            'class_report': model.class_report
+        }
+
+        return jsonify(result_dict)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
